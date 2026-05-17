7b26a46a23d93dab21216152eca42e074bf2d179
OwenACurran <owen@sww.co.nz>
2026-05-18 09:56:06 +1200

    Change

--- route_maker.py
+++ route_maker.py
@@ -3,20 +3,16 @@ import pygame as py
 
 py.init()
 
-# Set up the display
-width, height = 800, 600
+# ----------Set up the display-----------
+display_width, display_height = 800, 600
 screen = py.display.set_mode(
-    (width, height),
+    (display_width, display_height),
     py.RESIZABLE
 )
 py.display.set_caption("Route Maker")
 
 
-holds_panel_width = 20
-holds_panel_height = 20
-holds_panel = py.Surface((holds_panel_width, holds_panel_height))
-
-# Class to create a grid of points on the screen
+# ---------Grid class-------------
 class Grid:
     def __init__(self, x, y, size=(10, 10)):
         self.x = x
@@ -25,10 +21,11 @@ class Grid:
 
     # Update grid cell positions
     def update(self, width = py.display.get_window_size()[0], height = py.display.get_window_size()[1]):
+        global holds_panel_height
 
         # Calculate the gap between cells based on the new window size
         self.width = width 
-        self.height = height
+        self.height = (height - holds_panel_height)
         self.gapx = self.width // self.size[0]
         self.gapy = self.height // self.size[1]
         self.gap = (min(self.gapx, self.gapy))
@@ -43,12 +40,12 @@ class Grid:
             for h in range(self.size[1]):  
                 self.grid.append((self.x + w * self.gap, self.y + h * self.gap))
 
-grid = Grid(0, 0, (20, 10)) # Create a grid with 20 columns and 10 rows
+grid = Grid(0, 0, (25, 25)) 
 
-# Main loop
+# --------------Main loop-------------- 
 run = True
 while run:
-    # Event handling
+    # ----------Event handling-------------
     for event in py.event.get():
         if event.type == py.QUIT:
             run = False
@@ -57,30 +54,64 @@ while run:
             run = False
 
         elif event.type == py.VIDEORESIZE:
-            width, height = event.size
+            display_width, display_height = event.size
+
+    # ---- Mouse ----
+
+    if py.mouse.get_pressed() == True:
+        mouse_down = True
+    else:
+        mouse_down = False
  
-    width, height = py.display.get_window_size()
+    
 
     # -----------Panel-------------
+    display_width, display_height = py.display.get_window_size()
+    panel_surface_pos = 
 
     # Panel surface
-    holds_panel_width = width
-    holds_panel_height = 50
+    holds_panel_width = display_width
+    holds_panel_height = 100
     holds_panel = py.Surface((holds_panel_width, holds_panel_height))
-    holds_panel.fill((200, 200, 200)) # Fill the panel with a color
+    holds_panel.fill(("#c8c8c8"))
 
-
-    screen.fill((255, 255, 255))
+    # Resizing with mouse
+    mouse_grab_area = py.Rect(0, 0, holds_panel_width, 20).move(0, )
     
-    grid.update(width, height)
+
+    if mouse_grab_area.collidepoint(py.mouse.get_pos()):
+        ready_to_drag = True
+        print("true")
+    else:
+        ready_to_drag = False
+        print("false")
+
+    # if ready_to_drag == True:
+    #     py.mouse.set_cursor(py.SYSTEM_CURSOR_SIZENESW)
+    #     print("ready")
+
+    # else:
+    #     py.mouse.set_cursor(py.SYSTEM_CURSOR_ARROW)
 
     
+    
+    
+
+    # Drawing on the panel
+    py.draw.line(holds_panel, ("#000000"), (0, 0), (holds_panel_width, 0), 2)
+    
+    grid.update(display_width, display_height)
+    
+    # -----------Drawing for the main screen-------------
+    screen.fill(("#ffffffff"))
+
 
     for cell in grid.grid:
-        py.draw.circle(screen, (0, 0, 0), cell, 2)
+        py.draw.circle(screen, ("#000000"), cell, 2)
 
     
-    print(height)
-    screen.blit(holds_panel, (0, height - holds_panel_height)) # Draw the panel on the screen
 
+    # ------------------------     
+    # Update the display
+    screen.blit(holds_panel, (0, display_height - holds_panel_height))
     py.display.flip()

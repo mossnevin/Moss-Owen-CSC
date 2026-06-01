b73a00a9033f8bd7b72c49858579a8c5d17143f1
OwenACurran <owen@sww.co.nz>
2026-06-02 10:48:28 +1200

    Big change to holds

--- route_maker.py
+++ route_maker.py
@@ -1,5 +1,7 @@
 
 import pygame as py
+import os, json as js
+
 
 py.init()
 
@@ -10,6 +12,10 @@ screen = py.display.set_mode(
     py.RESIZABLE
 )
 py.display.set_caption("Route Maker")
+
+#holds panel dimensions
+holds_panel_width = display_width
+holds_panel_height = 100
 
 
 # ---------Grid class-------------
@@ -40,7 +46,30 @@ class Grid:
             for h in range(self.size[1]):  
                 self.grid.append((self.x + w * self.gap, self.y + h * self.gap))
 
-grid = Grid(0, 0, (25, 25)) 
+grid = Grid(0, 0, (25, 25))
+
+# --------------Holds dictionary and size, colour, and type variables-------------- 
+with open("inventory.json", "r") as f:
+    holds_data = js.load(f)
+
+size_properies = {
+    "Small": 10,
+    "Medium": 20,
+    "Large": 30
+}
+
+colour_properies = {
+    "Black": "#000000",
+    "Blue": "#0000ff",
+    "Red": "#ff0000",
+    "Green": "#31b431",
+    "Yellow": "#ffff00",
+    "Purple": "#BD5DBD",
+    
+}
+
+
+
 
 # --------------Main loop-------------- 
 run = True
@@ -58,50 +87,55 @@ while run:
 
     # ---- Mouse ----
 
-    if py.mouse.get_pressed() == True:
-        mouse_down = True
-    else:
-        mouse_down = False
+    mouse_down = py.mouse.get_pressed()[0]
  
     
 
     # -----------Panel-------------
     display_width, display_height = py.display.get_window_size()
-    panel_surface_pos = 
+    panel_surface_pos = screen
 
     # Panel surface
     holds_panel_width = display_width
-    holds_panel_height = 100
     holds_panel = py.Surface((holds_panel_width, holds_panel_height))
     holds_panel.fill(("#c8c8c8"))
 
     # Resizing with mouse
-    mouse_grab_area = py.Rect(0, 0, holds_panel_width, 20).move(0, )
+    
+    mouse_grab_area = py.Rect(0, display_height - holds_panel_height - 5, holds_panel_width, 10)
     
 
     if mouse_grab_area.collidepoint(py.mouse.get_pos()):
         ready_to_drag = True
-        print("true")
     else:
         ready_to_drag = False
-        print("false")
 
-    # if ready_to_drag == True:
-    #     py.mouse.set_cursor(py.SYSTEM_CURSOR_SIZENESW)
-    #     print("ready")
+    if ready_to_drag:
+        py.mouse.set_cursor(py.SYSTEM_CURSOR_SIZENS)
+    else:
+        py.mouse.set_cursor(py.SYSTEM_CURSOR_ARROW)
 
-    # else:
-    #     py.mouse.set_cursor(py.SYSTEM_CURSOR_ARROW)
-
-    
-    
-    
+    print(mouse_down, ready_to_drag)
+    if mouse_down and ready_to_drag:
+        new_mouse_y = py.mouse.get_pos()[1]
+        new_height = display_height - new_mouse_y
+        min_h = 20
+        max_h = max(40, display_height - 40)
+        holds_panel_height = max(min_h, min(max_h, new_height))
+        
 
     # Drawing on the panel
     py.draw.line(holds_panel, ("#000000"), (0, 0), (holds_panel_width, 0), 2)
     
     grid.update(display_width, display_height)
     
+    #Diplay holds on the panel
+    for hold in holds_data:
+        hold_size = size_properies[hold["size"]]
+        hold_colour = colour_properies[hold["colour"]]
+        py.draw.circle(holds_panel, hold_colour, (hold_size, holds_panel_height // 2), hold_size // 2)
+
+
     # -----------Drawing for the main screen-------------
     screen.fill(("#ffffffff"))
 

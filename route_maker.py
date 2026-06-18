bcc3a63056f0bcd01eb2e21a35ca08b494b7879b
OwenACurran <owen@sww.co.nz>
2026-06-18 22:48:53 +1200

    Bigg change

--- route_maker.py
+++ route_maker.py
@@ -1,11 +1,10 @@
 import pygame as py
-import os
+from dataclasses import dataclass, field
 import json as js
-import math
 
 py.init()
 
-# ----------Set up the display-----------  Change
+# ----------Set up the display-----------
 display_width, display_height = 800, 600
 screen = py.display.set_mode(
     (display_width, display_height),
@@ -18,10 +17,15 @@ holds_panel_width = display_width
 holds_panel_height = 100
 
 # Dragging state
-is_dragging = False
+mouse_down = False
 SPACING_ON_HOLDS_PANEL = 80
 ROOM_FOR_BUTTONS = 200
 on_row = 0
+
+#stuff
+font = py.font.SysFont(None, 36)
+clear_button = font.render("Clear", True, "Black")
+clear_button_rect = clear_button.get_rect()
 
 #---------Image loading---------
 try:
@@ -30,9 +34,13 @@ try:
     left_arrow = py.transform.flip(right_arrow, True, False)
 except py.error:
     print("Failed to load image")
-    
 
-# ---------Grid class-------------
+#---Function for lengths of vectors (no sqrt for efficiency)---
+def length_from_mouse_pos(v):
+    mouse = py.mouse.get_pos()
+    return (v[0] - mouse[0]) ** 2 + (v[1] - mouse[1]) ** 2 # pythagorean theorem but without the square root for efficiency
+
+# ---------Grid class----------
 class Grid:
     def __init__(self, x, y, size=(10, 10)):
         self.x = x
@@ -62,7 +70,39 @@ class Grid:
 
 grid = Grid(0, 0, (25, 25))
 
+#--------Making Classes-----------
+@dataclass
+class ProcessedHolds:
+    size: int
+    colour: str
+    x : int
+    y : int
+    row : int
+
+@dataclass
+class DisplayedHolds:
+    x : int
+    y : int
+    colour : str
+    size : int
+    rect : py.Rect = field(init = False)
+    
+    def __post_init__(self):
+        self.rect = py.Rect(0, 0, self.size * 2, self.size * 2)
+        self.rect.center = (self.x, self.y)
+
+class HoldDragging:
+    def __init__(self, hold_clicked: DisplayedHolds):
+        self.x = hold_clicked.x
+        self.y = hold_clicked.y
+        self.colour = hold_clicked.colour
+        self.size = hold_clicked.size
+
+
+
 # --------------Holds dictionary and size, colour, and type variables-------------- 
+with open("inventory.json", "r") as f:
+    holds_data = js.load(f)
 
 size_properies = {
     "Tiny": 5,
@@ -73,7 +113,7 @@ size_properies = {
 
 colour_properies = {
     "Black": "#000000",
-    "Blue": "#107cc4",
+    "Blue": "#0000ff",
     "Red": "#ff0000",
     "Green": "#31b431",
     "Yellow": "#ffff00",
@@ -81,14 +121,21 @@ colour_properies = {
     "Orange": "#FF9100"
 }
 
+drag_hold = False
+holds_on_grid: list[DisplayedHolds] = []
+
 # --------------Main loop-------------- 
 run = True
 while run:
-    with open("inventory.json", "r") as f:
-        holds_data = js.load(f)
+
+    #--------Clear button---------
+    box_round_clear_button_rect = py.Rect(0,0,100,50)
+    box_round_clear_button_rect.center = clear_button_rect.center
+    clear_button_rect.center = (holds_panel_width - 180, holds_panel_height // 2)
 
 
-    processed_holds_data = []
+
+
     rows_visible = holds_panel_height // 70
 
     usable_width = holds_panel_width - ROOM_FOR_BUTTONS
@@ -97,21 +144,45 @@ while run:
     if holds_per_row < 1:
         holds_per_row = 1 
 
+    processed_holds_data: list[ProcessedHolds] = []
 
     for i, hold in enumerate(holds_data):
         row_index = i // holds_per_row
         col_index = i % holds_per_row
 
-        processed_holds_data.append({
-            "size": size_properies[hold["size"]],
-            "colour": colour_properies[hold["colour"]],
-            "x": col_index * SPACING_ON_HOLDS_PANEL + 40,
-            "y": holds_panel_height // 2,
-            "row": row_index
-        })
-    
-    possible_rows = (processed_holds_data[-1]["row"] + 1) if processed_holds_data else 1
+        
+        processed_holds_data.append(
+            ProcessedHolds(
+                size = size_properies[hold["size"]],
+                colour = colour_properies[hold["colour"]],
+                x = col_index * SPACING_ON_HOLDS_PANEL + 40,
+                y = holds_panel_height // 2,
+                row = row_index
+            )
+        )
+
+    possible_rows = (processed_holds_data[-1].row + 1) if processed_holds_data else 1
     possible_rows = max(1, possible_rows)
+
+    displayed_holds: list[DisplayedHolds] = []
+    for hold in processed_holds_data:
+
+        slot = (hold.row - on_row) % possible_rows
+
+        if slot < rows_visible:
+            
+
+            displayed_holds.append(
+                DisplayedHolds(
+                    x = hold.x,
+                    y = (holds_panel_height // (rows_visible + 1)) * (slot + 1),
+                    colour = hold.colour,
+                    size = hold.size
+                )
+            )
+            
+            
+ 
     
 
     # ----------Event handling-------------
@@ -121,48 +192,66 @@ while run:
     l_arrow_rect = left_arrow.get_rect(center =(holds_panel_width - 90, holds_panel_height // 2))
     r_arrow_rect = right_arrow.get_rect(center = (holds_panel_width - 30, holds_panel_height// 2))
 
+    mouse_pos = py.mouse.get_pos()
+    
+    clicked_hold = False
     for event in py.event.get():
         if event.type == py.QUIT:
             run = False
         elif event.type == py.VIDEORESIZE:
-            display_width, display_height = event.size
+            display_width = max(event.size[0], 400)
+            display_height = max(event.size[1], 400)
+            screen = py.display.set_mode((display_width, display_height), py.RESIZABLE)
         elif event.type == py.MOUSEBUTTONDOWN:
             # Only start dragging if the click originates in the grab area
-            if mouse_grab_area.collidepoint(py.mouse.get_pos()):
-                is_dragging = True
-
-            # Check for arrow clicks
-            elif l_arrow_rect.move(0,display_height - holds_panel_height).collidepoint(py.mouse.get_pos()):
+            if mouse_grab_area.collidepoint(mouse_pos):
+                mouse_down = True
+            elif l_arrow_rect.move(0,display_height - holds_panel_height).collidepoint(mouse_pos):
                 on_row = (on_row - 1) % possible_rows
-            elif r_arrow_rect.move(0,display_height - holds_panel_height).collidepoint(py.mouse.get_pos()):
+
+            elif r_arrow_rect.move(0,display_height - holds_panel_height).collidepoint(mouse_pos):
                 on_row = (on_row + 1) % possible_rows
 
-            # Check for hold clicks
-            for hold in processed_holds_data:
-                hold_rect = py.Rect(0, 0, hold["size"]*2, hold["size"]*2)
-                hold_rect.center = (hold["x"], hold["y"])
-                hold_rect.move_ip(0, display_height - holds_panel_height)
-                if hold_rect.collidepoint(py.mouse.get_pos()):
-                    print(f"Clicked on hold: {hold}")
-                    break
+            elif box_round_clear_button_rect.move(0,display_height - holds_panel_height).collidepoint(mouse_pos):
+                holds_on_grid.clear()
 
+            else:
+                for hold in displayed_holds:
+                    if hold.rect.move(0,display_height - holds_panel_height).collidepoint(mouse_pos):
+                        clicked_hold = hold
+                        break
+                if clicked_hold:
+                    drag_hold = HoldDragging(clicked_hold)
+                    drag_hold.y -= (display_height - holds_panel_height) #Adjust for the y being too high
+                    
+        elif event.type == py.MOUSEMOTION:
+            if drag_hold:
+                if grid.x <= event.pos[0] <= grid.x + grid.gap * (grid.size[0] - 1) and grid.y <= event.pos[1] <= grid.y + grid.gap * (grid.size[1] - 1):
+                        drag_hold.x, drag_hold.y = min(grid.grid, key = length_from_mouse_pos)
+
+                else: drag_hold.x, drag_hold.y = event.pos
+                
         elif event.type == py.MOUSEBUTTONUP:
-            is_dragging = False
+            if drag_hold:
+                holds_on_grid.append(drag_hold)
+                
+            drag_hold = False
+            mouse_down = False
 
-
+    
     # -----------Panel-------------
     holds_panel_width = display_width
     holds_panel = py.Surface((holds_panel_width, holds_panel_height))
     holds_panel.fill(("#c8c8c8"))
 
     # Cursor feedback
-    if mouse_grab_area.collidepoint(py.mouse.get_pos()):
+    if mouse_grab_area.collidepoint(mouse_pos):
         py.mouse.set_cursor(py.SYSTEM_CURSOR_SIZENS)
     else:
         py.mouse.set_cursor(py.SYSTEM_CURSOR_ARROW)
 
-    if is_dragging:
-        new_mouse_y = py.mouse.get_pos()[1]
+    if mouse_down:
+        new_mouse_y = mouse_pos[1]
         new_height = display_height - new_mouse_y
         min_h = 70
         max_h = 70 * possible_rows
@@ -175,22 +264,14 @@ while run:
     grid.update(display_width, display_height)
     
     # Display holds on the panel
-    for hold in processed_holds_data:
-
-        slot = (hold["row"] - on_row) % possible_rows
-
-        if slot < rows_visible:
-
-            x = hold["x"]
-            y = (holds_panel_height // (rows_visible + 1)) * (slot + 1)
-            colour = hold["colour"]
-            size = hold["size"]
+    
+    for hold in displayed_holds:
 
             py.draw.circle(
                 holds_panel, 
-                colour, 
-                (x, y), 
-                size
+                hold.colour, 
+                (hold.x, hold.y), 
+                hold.size
             )
 
     # Buttons
@@ -203,6 +284,28 @@ while run:
     for cell in grid.grid:
         py.draw.circle(screen, ("#000000"), cell, 2)
 
+    if drag_hold:
+        py.draw.circle(screen,
+                        color = drag_hold.colour, 
+                        center = (drag_hold.x, drag_hold.y),
+                        radius = drag_hold.size
+                        )
+    
+    if holds_on_grid:
+        for hold in holds_on_grid:
+            py.draw.circle(screen,
+                        color = hold.colour, 
+                        center = (hold.x, hold.y),
+                        radius = hold.size
+                        )
+    
+    
+    py.draw.rect(holds_panel, (255, 255, 255), box_round_clear_button_rect, 3)
     # Update the display
+
+    
+    holds_panel.blit(clear_button, clear_button_rect)
     screen.blit(holds_panel, (0, display_height - holds_panel_height))
+
+    
     py.display.flip()
